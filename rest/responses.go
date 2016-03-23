ca70717e5e42be236ad35af53a73b0ce2aa8ecb4
Joao Pedro Evangelista <evangelistajoaop@gmail.com>
2016-03-23 19:38:45 -0300

    Polish

--- rest/responses.go
+++ rest/responses.go
@@ -1,18 +1,18 @@
 package rest
 
-// AddressResponse wrapps a json reponse for single address
+// AddressResponse wraps a json reponse for single address
 type AddressResponse struct {
 	Address string      `json:"address"`
 	Error   NestedError `json:"errors"`
 }
 
-// LocationResponse wrapps a json response for a single location
+// LocationResponse wraps a json response for a single location
 type LocationResponse struct {
 	Location Location    `json:"location"`
 	Error    NestedError `json:"error"`
 }
 
-// NestedError wrapps a error struc to inform any errors occured when communiting with API
+// NestedError wraps a error struc to inform any errors occured when communiting with API
 type NestedError struct {
 	Status       string `json:"status"`
 	ErrorMessage string `json:"error_message"`
