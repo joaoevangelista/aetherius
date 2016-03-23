3009a23a93c17b12bfd5e12e342a1dee0797f7f0
Joao Pedro Evangelista <evangelistajoaop@gmail.com>
2016-03-23 19:37:50 -0300

    Adds response errors on AddressResponse in case of no results are returned. Fixes gh-1

--- rest/extractors.go
+++ rest/extractors.go
@@ -10,12 +10,12 @@ func ExtractLocation(gr *GeoResponse) LocationResponse {
 	return LocationResponse{Location: Location{}, Error: NestedError{Status: gr.Status, ErrorMessage: gr.ErrorMessage}}
 }
 
-// ExtractAddress gets the FormattedAddress on GeoResponse and wrapps it into a AddressResponse
+// ExtractAddress gets the FormattedAddress on GeoResponse and wraps it into a AddressResponse
 func ExtractAddress(gr *GeoResponse) AddressResponse {
 	results := gr.Results
 	if len(results) >= 1 {
 		result := results[0]
 		return AddressResponse{Address: result.FormattedAddress}
 	}
-	return AddressResponse{}
+	return AddressResponse{Address: Address{}, Error:NestedError{Status: gr.Status, ErrorMessage:gr.ErrorMessage}}
 }
